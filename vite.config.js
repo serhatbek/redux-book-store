1878fce4f5a7ddca704bc936e4e2a3f80eba31c7
serhatbek <serhat.bekk@gmail.com>
2023-10-23 02:04:10 +0300

    edit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/redux-book-store/',
+  base: 'dist',
   plugins: [react()],
   build: {
     outDir: 'dist',
